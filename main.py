37562a45e806437ba524c7cda0d342263864f846
Nova <nova@cutey.com>
2025-10-28 02:11:04 +0000

    got navbar to a functional state

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    return flask.render_template('index.html', stylesheets="index.css")
+
+@app.route("/portfolio")
+def portfolio():
+    pass
+
+@app.route("/commissions")
+def commissions():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
